37f4069ea55667ba4eafa077e5da6a4f532b395f
ItsZipzon <rune.molander@hotmail.com>
2024-01-27 20:20:44 +0100

    Added files to put header, footer and home page inside

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,7 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useActionData } from "react-router-dom";
 import "./Header.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Header() {
+
+    const [logo, setLogo] = useState('');
+
+    useEffect(() => {
+        setLogo("http://localhost:8080/test/image/logo.png");
+    }, []);
+
     return(
         <div className="Header">
             <Link to="/" className="headerLink" >
@@ -10,6 +19,7 @@ function Header() {
             <Link to="/test" className="headerLink" >
                 <div>Test</div>
             </Link>
+            <img src={logo} alt="logo" className="headerLogo"/>
         </div>
     );
 }
