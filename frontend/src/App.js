9e02ad6c72e76731e5e9a4013479af255cf58f31
ItsZipzon <rune.molander@hotmail.com>
2024-01-27 20:06:29 +0100

    Added files to put header, footer and home page inside

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import Test from './pages/Test/Test';
 import { Route, Routes } from 'react-router';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Test/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/test' element={<Test/>} />
       </Routes>
     </>
   );
--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return(
         <div className="Header">
-
+            <Link to="/test" className="testLink" >
+                <div>Test</div>
+            </Link>
         </div>
     );
 }
