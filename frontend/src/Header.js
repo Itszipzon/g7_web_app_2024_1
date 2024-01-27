eca85d953db3a6459671c0b43e5e8171c781064b
ItsZipzon <rune.molander@hotmail.com>
2024-01-27 20:08:59 +0100

    Added files to put header, footer and home page inside

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
     return(
         <div className="Header">
+            <Link to="/" className="testLink" >
+                <div>Home</div>
+            </Link>
             <Link to="/test" className="testLink" >
                 <div>Test</div>
             </Link>
