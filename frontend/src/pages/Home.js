bd338c4d46dd6b62e9b4b172f0fc6d2b21dc14b3
runemola <runemola@stud.ntnu.no>
2024-02-06 09:59:11 +0100

    Added some style to home page to help with setup later

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,9 @@ import "./css/Home.css";
 function Home() {
     return(
         <div className="Home">
-            
+            <div className="homeContainer">
+
+            </div>
         </div>
     );
 }
