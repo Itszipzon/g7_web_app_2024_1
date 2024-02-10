e8f2eed135962f947f4f010ff74417bfd316faa0
ItsZipzon <rune.molander@hotmail.com>
2024-02-10 11:56:35 +0100

    Added list showing up on location input, Also added display on open or close

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,11 +6,16 @@ function Home() {
     const [carsearchItems, setCarsearchItems] = useState([]);
     const [carNameValue, setCarNameValue] = useState("");
 
-    const handleInputFocus = () => {
+    
+    const [LocationinputMarked, setLocationinputMarked] = useState(false);
+    const [LocationsearchItems, setLocationsearchItems] = useState([]);
+    const [LocationValue, setLocationValue] = useState("");
+
+    const handleCarNameInputFocus = () => {
         setCarinputMarked(true);
     }
 
-    const handleInputBlur = () => {
+    const handleCarNameInputBlur = () => {
         setTimeout(() => {
             setCarinputMarked(false);
         }, 110);
@@ -18,6 +23,20 @@ function Home() {
 
     const handleCarNameClick = (e) => {
         setCarNameValue(e);
+    }
+
+    const handleLocationInputFocus = () => {
+        setLocationinputMarked(true);
+    }
+
+    const handleLocationInputBlur = () => {
+        setTimeout(() => {
+            setLocationinputMarked(false);
+        }, 110);
+    }
+
+    const handleLocationClick = (e) => {
+        setLocationValue(e);
     }
 
     const carList = useMemo(() => [
@@ -48,13 +67,100 @@ function Home() {
         },
     ], []) 
 
+    const locationList = useMemo(() => [
+        {
+            name: "XY 7-Eleven Ysteneset",
+            street: "Sundgata 22",
+            postalCode: "6003",
+            state: "Ålesund",
+            country: "Norway",
+            open: [ 
+                "07-23",
+                "07-23",
+                "07-23",
+                "07-23",
+                "07-23",
+                "07-23",
+                "07-23"
+            ]
+        },
+        {
+            name: "Moa",
+            street: "Moavegen 1",
+            postalCode: "6018",
+            state: "Ålesund",
+            country: "Norway",
+            open: [ 
+                "Closed",
+                "08-22",
+                "08-22",
+                "08-22",
+                "08-22",
+                "08-22",
+                "08-20",
+            ]
+        },
+        {
+            name: "Obs bygg",
+            street: "Smibakken 2",
+            postalCode: "6018",
+            state: "Ålesund",
+            country: "Norway",
+            open: [ 
+                "Closed",
+                "07-21",
+                "07-21",
+                "07-21",
+                "07-21",
+                "07-21",
+                "09-19",
+            ]
+        },
+        {
+            name: "Møller bil Trondheim",
+            street: "Ingvald Ystgaards veg 21",
+            postalCode: "7047",
+            state: "Trondheim",
+            country: "Norway",
+            open: [ 
+                "Closed",
+                "07:30-16:30",
+                "07:30-16:30",
+                "07:30-16:30",
+                "07:30-16:30",
+                "07:30-16:30",
+                "10:30-14:30",
+            ]
+        },
+        {
+            name: "Pilot utleie AS",
+            street: "Heggstadmoen 8",
+            postalCode: "7080",
+            state: "Heimdal",
+            country: "Norway",
+            open: [ 
+                "Closed",
+                "07-16",
+                "07-16",
+                "07-16",
+                "07-16",
+                "07-16",
+                "Closed",
+            ]
+        }
+    ], []);
+
+    const d = new Date();
+    let day = d.getDay();
+
     let validCarNames = [];
 
     useEffect(() => {
+        setLocationsearchItems(locationList);
         setCarsearchItems(carList);
-    }, [carList]);
+    }, [carList, locationList]);
 
-    const handleSearchInputChange = (e) => {
+    const handleCarNameSearchInputChange = (e) => {
         let searchTerm = e.target.value;
         let validItemCounter = 0;
         validCarNames = [];
@@ -69,6 +175,23 @@ function Home() {
         setCarNameValue(searchTerm);
     }
 
+    var validLocation = [];
+
+    const handleLocationSearchInputChange = (e) => {
+        let searchTerm = e.target.value;
+        let validItemCounter = 0;
+        validLocation = [];
+        for (let i = 0; i < locationList.length; i++) {
+            if (locationList[i].name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                validLocation[validItemCounter] = locationList[i];
+                validItemCounter++;
+            }
+        }
+        validLocation.sort((a, b) => customSort(a, b, searchTerm));
+        setLocationsearchItems(validLocation);
+        setLocationValue(searchTerm);
+    }
+
     const handleSearchButtonClick = () => {
 
     }
@@ -78,7 +201,7 @@ function Home() {
             <div className="homeContainer">
                 <div className="searchForCarContainer">
                     <div style={{ "width": "240px" }}>
-                        <input type="text" placeholder="Car name" value={carNameValue} onFocus={handleInputFocus} onBlur={handleInputBlur} onChange={handleSearchInputChange} />
+                        <input type="text" placeholder="Car name" value={carNameValue} onFocus={handleCarNameInputFocus} onBlur={handleCarNameInputBlur} onChange={handleCarNameSearchInputChange} />
                         <div className="searchContentContainer" style={carinputMarked ? { "display": "flex" } : { "display": "none" }}>
                             <ul className="searchContentHomeUl">
                                 {carsearchItems.map((item, index) =>
@@ -93,11 +216,32 @@ function Home() {
                         </div>
                     </div>
                     <div style={{ "width": "240px" }}>
-                        <input type="text" placeholder="Location" />
+                        <input type="text" placeholder="Location" value={LocationValue} onFocus={handleLocationInputFocus} onBlur={handleLocationInputBlur} onChange={handleLocationSearchInputChange} />
+                        <div className="searchContentContainer" style={LocationinputMarked ? { "display": "flex" } : { "display": "none" }}>
+                            <ul className="searchContentHomeUl">
+                                {LocationsearchItems.map((item) =>
+                                    <li className="searchContentHomeLi" key={item.street + ", " + item.postalCode + " " + item.state} onClick={() => handleLocationClick(item.name)} >
+                                        <div className="searchHomeNameContainer">
+                                            <div className="searchHomeLocationContainer">
+                                                <div className="searchHomeLocationContainerTop">
+                                                    {item.name}
+                                                </div>
+                                                <div className="searchHomeLocationContainerMid">
+                                                    {item.street + ", " + item.postalCode + " " + item.state}
+                                                </div>
+                                                <div className="searchHomeLocationContainerBot">
+                                                    {(open(item.open[day]) ? "🟢" : "🔴") + item.open[day]}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
                     </div>
                     <input type="date" />
                     <div className="searchButton" style={{ "marginLeft": "10px", "marginTop": "-2px" }} onClick={handleSearchButtonClick}>Search</div>
-
+                    
                 </div>
             </div>
         </div>
@@ -114,4 +258,39 @@ function customSort(a, b, searchString) {
     } else {
         return a.name.localeCompare(b.name);
     }
+}
+
+function open(timeAsString) {
+    if (timeAsString.toLowerCase() === "closed") return false;
+    const open = timeAsString.split("-")[0];
+    const closed = timeAsString.split("-")[1];
+
+    let openNow = false;
+
+    let date = new Date();
+    let time = date.getHours();
+    let minute = date.getMinutes();
+    
+    if (closed.includes(":") || open.includes(":")) {
+        let closedTotal = closed.split(":");
+        let openTotal = open.split(":");
+
+        if (Number(openTotal[0]) > time || Number(closedTotal[0]) < time) {
+            return false;
+        } else if (Number(openTotal[0]) < time && Number(closedTotal[0]) < time) {
+            return true;
+        } else if ((Number(openTotal[0]) === time && Number(openTotal[1]) > minute) 
+            || (Number(closedTotal[0]) === time && Number(closedTotal[1]) < minute)) {
+                return false;
+        } else {
+            return true;
+        }
+    } else {
+        if (Number(closed) > time && Number(open) < time) {
+            openNow = true;
+        }
+    }
+
+    return openNow;
+
 }
