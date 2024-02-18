b5071a4cf91dbc9e4d1ffccda37ac814e53a2a12
ItsZipzon <rune.molander@hotmail.com>
2024-02-18 18:59:23 +0100

    Started on admin panel

---
+++ frontend/src/pages/admin/HomeAdminPage.js
@@ -0,0 +1,13 @@
+import './HomeAdminPage.css';
+
+function HomeAdminPage() {
+
+    return (
+        <div className="homeadminpage">
+            <h1>Admin Home Page</h1>
+        </div>
+    );
+
+}
+
+export default HomeAdminPage;
