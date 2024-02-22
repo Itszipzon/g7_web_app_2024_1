387d5a00673b8bc68436a47c0454a5725cfe7600
ItsZipzon <rune.molander@hotmail.com>
2024-02-22 21:20:03 +0100

    Added all the image names of VolksWagen Golf

--- Database/InsertCars.sql
+++ Database/InsertCars.sql
@@ -1,4 +1,4 @@
-INSERT INTO Car (Maker, Model, Year, Fuel, Transmission, Seats, Extras)
+INSERT INTO Car (Maker, Model, Year, Fuel, Transmission, Seats, Extras, Images)
 VALUES (
     "VolksWagen",
     "Golf",
@@ -6,7 +6,8 @@ VALUES (
     "Diesel",
     "Manual",
     5,
-    "Bluetooth, DAB radio, Warming in chairs"
+    "Bluetooth, DAB radio, Warming in chairs",
+    "895_714866005.jpg, 895_1710930786.jpg, 895_1720093311.jpg, 895_1294831697.jpg, 895_1400829524.jpg, 895_326360500.jpg, 895_1612184175.jpg, 895_1663729981.jpg, 895_1871290765.jpg, 895_110028289.jpg, 895_162126435.jpg, 895_1771880685.jpg, 895_492597128.jpg, 895_119503709.jpg, 895_1647294634.jpg, 895_2088234816.jpg, 895_434012345.jpg, 895_122160674.jpg, 895_760842343.jpg, 895_2040929032.jpg, 895_251989209.jpg, 895_1539323279.jpg, 895_157732232.jpg, 895_1298772323.jpg, 895_609324874.jpg, 895_1113751452.jpg, 895_38062558.jpg, 895_413822693.jpg, 895_268949906.jpg, 895_1703132129.jpg, 895_1643795835.jpg"
 );
 
 INSERT INTO Car (Maker, Model, Year, Fuel, Transmission, Seats, Extras)
