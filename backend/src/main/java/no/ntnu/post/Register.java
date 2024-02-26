81c7cd64fae1b190bb7b2c8284947b1b5485d862
ItsZipzon <rune.molander@hotmail.com>
2024-02-26 18:57:11 +0100

    Checkstyle

--- backend/src/main/java/no/ntnu/post/Register.java
+++ backend/src/main/java/no/ntnu/post/Register.java
@@ -2,44 +2,56 @@ package no.ntnu.post;
 
 import java.security.SecureRandom;
 import java.util.Base64;
-
+import no.ntnu.DatabaseCon;
+import no.ntnu.user.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.ntnu.DatabaseCon;
-import no.ntnu.user.User;
-
+/**
+ * Class for registering a user.
+ */
 @RestController
 @RequestMapping("api/register")
 public class Register {
+
+  /**
+   * Registers a user.
+   *
+   * @param user the user to register
+   * @return a response entity
+   */
+  @PostMapping("user")
+  public ResponseEntity<String> registerUser(@RequestBody User user) {
+    byte[] salt = new byte[16];
+    SecureRandom random = new SecureRandom();
+
+    random.nextBytes(salt);
+
+    String saltString = Base64.getEncoder().encodeToString(salt);
+    User newUser = new User();
+
+    newUser.setEmail(user.getEmail());
+    newUser.setSalt(saltString);
+    newUser.setPassword(user.getPassword());
+    newUser.setTerms(user.isTerms());
+    newUser.setGuest(user.isGuest());
+    newUser.setAdmin(user.isAdmin());
+
+    String query = "INSERT INTO Users (Email, Password, Salt, Terms, IsGuest, IsAdmin) VALUES ('"
+        + newUser.getEmail()
+        + "', '" + newUser.getPassword() + "', '"
+        + saltString + "', "
+        + newUser.isTerms()
+        + ", " + newUser.isGuest()
+        + ", " + newUser.isAdmin() + ")";
     
-    @PostMapping("user")
-    public ResponseEntity<String> registerUser(@RequestBody User user) {
-        DatabaseCon db = new DatabaseCon();
-        byte[] salt = new byte[16];
-        SecureRandom random = new SecureRandom();
+    DatabaseCon db = new DatabaseCon();
 
-        random.nextBytes(salt);
-
-        String saltString = Base64.getEncoder().encodeToString(salt);
-        User newUser = new User();
-
-        newUser.setEmail(user.getEmail());
-        newUser.setSalt(saltString);
-        newUser.setPassword(user.getPassword());
-        newUser.setTerms(user.isTerms());
-        newUser.setGuest(user.isGuest());
-        newUser.setAdmin(user.isAdmin());
-
-
-        String query = "INSERT INTO Users (Email, Password, Salt, Terms, IsGuest, IsAdmin) VALUES ('" + newUser.getEmail() + "', '"
-                + newUser.getPassword() + "', '" + saltString + "', " + newUser.isTerms() + ", " + newUser.isGuest() + ", " + newUser.isAdmin() +")";
-
-        db.update(query);
-        return ResponseEntity.ok("User registered");
-    }
+    db.update(query);
+    return ResponseEntity.ok("User registered");
+  }
 
 }
