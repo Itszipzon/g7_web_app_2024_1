aedccc7587ba9a6012dbca1e41475e4d787d6de6
ItsZipzon <rune.molander@hotmail.com>
2024-03-01 08:52:27 +0100

    Started on backend register and login

--- backend/src/main/java/no/ntnu/api/UserApi.java
+++ backend/src/main/java/no/ntnu/api/UserApi.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import no.ntnu.DatabaseCon;
 import no.ntnu.user.SessionManager;
-
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
